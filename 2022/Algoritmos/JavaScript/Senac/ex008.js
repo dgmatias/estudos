9e17c373f87e64ce995041b4b5da32886074895d
Diego Matias <109382931+dgmatias@users.noreply.github.com>
2022-07-18 19:27:04 -0300

    Update ex008.js
    Nesse algoritmo pedimos 3 números ao usuário, e o programa irá imprimir os 3 números e irá imprimir o maior número entre eles

--- 2022/Algoritmos/JavaScript/Senac/ex008.js
+++ 2022/Algoritmos/JavaScript/Senac/ex008.js
@@ -1,7 +1,9 @@
 var n1 = Number(prompt('Digite o primeiro número'));
 var n2 = Number(prompt('Digite o segundo número'));
 var n3 = Number(prompt('Digite o terceiro número'));
+
 var maior = n1;
+
 if (n2 > maior) maior = n2;
 if (n3 > maior) maior = n3;
 document.writeln (`Entre os números ${n1}, ${n2} e ${n3} o maior entre eles é : ${maior}`);
