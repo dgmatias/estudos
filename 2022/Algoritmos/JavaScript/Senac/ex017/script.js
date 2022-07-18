6ebc08fbca13959e0ee59cb68d003a2767e064ef
Diego Matias <109382931+dgmatias@users.noreply.github.com>
2022-07-18 17:01:24 -0300

    Update script.js

--- 2022/Algoritmos/JavaScript/Senac/ex017/script.js
+++ 2022/Algoritmos/JavaScript/Senac/ex017/script.js
@@ -1,23 +1,17 @@
 var continuar = false;
 while (continuar === false) {
+  
   var limiteInf = Number(prompt('Digite um número'));
   var limiteSup = Number(prompt('Digite outro número'));
-
+  
   if (limiteInf > limiteSup) {
     continuar = confirm ('O primeiro número é maior que o segundo deseja continuar assim mesmo ?');
-  }
-  if (continuar === true) {
-    aux = limiteSup;
-    limiteSup = limiteInf;
-    limiteInf = aux;
-    continuar === true
-    break;
-  }
-  
-  if (limiteInf < limiteSup) {
-    continuar === true;
-    break;
-  } 
+    aux = limiteInf;
+    limiteInf = limiteSup;
+    limiteSup = aux;
+  } else {
+      break;
+    } 
 }
 while (limiteInf <= limiteSup) {
     if (limiteInf % 2 === 0) {
