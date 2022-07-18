2996858de68a2cb803978224f2efb5939827b09a
Diego Matias <109382931+dgmatias@users.noreply.github.com>
2022-07-18 19:35:17 -0300

    Create ex014.js

---
+++ 2022/Algoritmos/JavaScript/Senac/ex014.js
@@ -0,0 +1,26 @@
+var num = Number(prompt('Digite um número'))
+switch (num) {
+    case 1: 
+        alert ('Domingo');
+        break;
+    case 2: 
+        alert ('Segunda');
+        break;
+    case 3:
+         alert ('Terça');
+        break;
+    case 4:
+         alert ('Quarta');
+        break;
+    case 5: 
+        alert ('Quinta');
+        break;
+    case 6:
+         alert ('Sexta');
+        break;
+    case 7: 
+        alert ('Sábado');
+        break;
+default:
+        alert ('Digite um número válido')
+}
